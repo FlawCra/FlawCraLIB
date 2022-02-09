3aa1d52f07186212e4cd1fc7bfe98031df007275
FlawCra <ildi251086@gmail.com>
2022-02-09 21:25:15 +0100

    Dev (#11)
    * Fixed SoundcloudResolve
    
    * Added ForyxLIB.copyTextToClipboard
    
    + ForyxLIB.copyTextToClipboard
    
    * serverchange
    
    * versioning
    
    * fixed short
    
    * Update ForyxLIB.js
    
    * Updated Short Function
    
    * First step of migrating to automatic NPM publishing system
    
    * Create LICENSE (#10)
    
    * next step complete
    
    * Delete ForyxLIB.js
    
    Co-authored-by: FlawCra <office@flawcra.cc>

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin")
+const webpack = require('webpack');
+
+module.exports = {
+  entry: {
+    main: './src/FlawCraLIB.js',
+  },
+  output: {
+    filename: '[name].js',
+    path: __dirname + '/dist',
+  },
+  plugins: [
+    new NodePolyfillPlugin(),
+    new webpack.BannerPlugin({
+      banner: `FlawCraLIB
+@author  FlawCra <office@flawcra.cc>
+@license GPL-3.0-or-later`,
+    })
+  ],
+  resolve: {
+    fallback: {
+      "path": require.resolve("path-browserify"),
+      "fs": false
+    }
+  },
+};
