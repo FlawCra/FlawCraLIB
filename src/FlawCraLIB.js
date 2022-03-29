1b5c937004e9b5f1a1247e88cb7420e5e5906c17
Xanax-C-137 <moritz.fuessel@flawcra.cc>
2022-03-29 22:00:12 +0200

    Added JSDoc

--- src/FlawCraLIB.js
+++ src/FlawCraLIB.js
@@ -1,9 +1,16 @@
 export default class FlawCraLIB {
 	jQuery = require('jquery');
 	SCCID = "8df0d68fcc1920c92fc389b89e7ce20f";
+
+	/**
+	 * @param  {string} songURL
+	 * @description Get the stream URL of a Soundcloud song
+	 * 
+	 * @returns {string} stream URL
+	 */
 	static resolveSoundcloudSong(songURL) {
 		var result = null;
-		$.ajax({
+		jQuery.ajax({
 			url: "https://api.soundcloud.com/resolve/?url=" + songURL + "&client_id=" + SCCID,
 			type: 'get',
 			dataType: 'json',
@@ -15,6 +22,15 @@ export default class FlawCraLIB {
 		return result;
 	
 	};
+
+	/**
+	 * @param  {number} joinYear
+	 * @param  {number} joinMonth
+	 * @param  {number} tweetCount
+	 * @description Calculate an estimate of how many tweets a user posts per day
+	 * 
+	 * @returns {number} estimated tweet count
+	 */
 	static calcTweetsADay(joinYear, joinMonth, tweetCount) {
 		var joinDate = new Date(joinYear, joinMonth);
 		var nowDate = new Date();
@@ -23,9 +39,16 @@ export default class FlawCraLIB {
 		var tweetsADay = Math.round(tweetCount / Difference_In_Days);
 		return tweetsADay;
 	};
+
+	/**
+	 * @param  {string} shortURL
+	 * @description Shorten a URL using FlawCra URL Shortener
+	 * 
+	 * @returns {string} JSON response
+	 */
 	static shortURL(shortURL) {
 		var result = null;
-		$.ajax({
+		jQuery.ajax({
 			url: "https://cors.flawcra.cc/?" + "https://api.flawcra.cc/short/?url=" + shortURL,
 			type: 'get',
 			dataType: 'json',
@@ -37,16 +60,29 @@ export default class FlawCraLIB {
 		return result;
 	
 	};
+
+	/**
+	 * @param  {string} vidID
+	 * @description Get the thumbnail URL of a YouTube video
+	 * 
+	 * @returns {string} thumbnail URL
+	 */
 	static getYTThumbnailURL(vidID) {
-		$.get('https://img.youtube.com/vi/' + vidID + '/maxresdefault.jpg')
+		jQuery.get('https://img.youtube.com/vi/' + vidID + '/maxresdefault.jpg')
 			.done(function () {
 				return 'https://img.youtube.com/vi/' + vidID + '/maxresdefault.jpg';
 	
 			}).fail(function () {
 				return "Invalid Image";
-	
-			})
+			});
 	};
+
+	/**
+	 * @param  {string} text
+	 * @description Copy text to clipboard
+	 * 
+	 * @returns {boolean} true if successful, false if not
+	 */
 	static copyTextToClipboard(text) {
 		if (!navigator.clipboard) {
 			var elem = document.createElement('div');
@@ -58,17 +94,30 @@ export default class FlawCraLIB {
 			$temp.val(jQuery(elem).text()).select();
 			document.execCommand("copy");
 			$temp.remove();
-			return;
+			return true;
 		}
 		navigator.clipboard.writeText(text).then(function () {
-			console.log('Async: Copying to clipboard was successful!');
+			return true;
 		}, function (err) {
-			console.error('Async: Could not copy text: ', err);
+			return false;
 		});
 	};
+
+	/**
+	 * @param  {string} str
+	 * @description Reverse a given string
+	 * 
+	 * @returns {string} reversed string
+	 */
 	static reverseString(str) {
 		return str.split("").reverse().join("");
 	};
+
+	/**
+	 * @param  {string} url
+	 * @param  {method} callback=null
+	 * @description Fetch and execute a script from a given URL and execute a callback function
+	 */
 	static loadJS(url, callback = null) {
 		fetch("https://cors.flawcra.cc/?" + url).then(res => res.text().then(txt => {
 			eval(txt);
